a07e6e6aaca420281e0abb1f60eb38535745f749
H.İbrahim Annadinç <h.ibrahim2321@gmail.com>
2022-10-14 12:33:41 +0300

    commit

---
+++ python_temelleri/string-methods.py
@@ -0,0 +1,26 @@
+
+from textwrap import indent
+
+
+message = 'Hello there. My name is Halil İbrahim Annadinç'
+
+# message = message.upper()
+# message = message.lower()
+# message = message.title()
+# message = message.capitalize()
+# message = message.strip()
+# message = message.split()
+# message = message.split('.')
+# message = '--'.join(message)
+
+# index = message.find('Halil')
+# isfound = message.startswith('B')
+# isfound = message.endswith('ç')
+# message = message.replace('Halil', 'Mustafa')
+# message = message.replace('ç', 'c').replace('İ', 'I')
+# message = message.center(100, '*')
+
+
+print(message)
+
+# print(message[0])
