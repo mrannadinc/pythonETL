6bf2c9ddda3992716ee2ac2a5b51d2229d98a17e
H.İbrahim Annadinç <h.ibrahim2321@gmail.com>
2024-05-28 01:12:13 +0300

    dbt-test

--- dbt-airflow-dag/dags/ilab-dbt-dag.py
+++ dbt-airflow-dag/dags/ilab-dbt-dag.py
@@ -8,20 +8,25 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash_operator import BashOperator
 
+PATH_TO_DBT_PROJECT = "/usr/local/airflow/dags/dbt/dbt_ilab"
+PATH_TO_DBT_VENV = "/usr/local/airflow/dbt_venv/bin/activate"
+
 with DAG(
     dag_id="ilab_dbt",
-    start_date=datetime(2022, 11, 27),
+    start_date=datetime(2024, 5, 27),
     schedule="@daily",
 ) as dag:
     
-    start = EmptyOperator(task_id="ingestion_workflow")
+    start = EmptyOperator(task_id="start")
 
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command='dbt run',
+        bash_command="source {0} && dbt run".format(PATH_TO_DBT_VENV),
+        env={"PATH_TO_DBT_VENV": PATH_TO_DBT_VENV},
+        cwd=PATH_TO_DBT_PROJECT,
         dag=dag
     )
 
-    end = EmptyOperator(task_id="some_extraction")
+    end = EmptyOperator(task_id="end")
 
     start >> dbt_run >> end
